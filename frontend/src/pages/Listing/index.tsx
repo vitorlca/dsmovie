e5b9e40d9f5fd807f6e3bf33730abcc9a8bba01e
vitorlca <vitorlcaraujo@gmail.com>
2022-03-13 15:10:26 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,11 +2,19 @@ import axios from 'axios';
 import Pagination from 'components/Pagination';
 import MovieCard from 'components/MovieCard';
 import { BASE_URL } from 'Utils/requests';
+import { useState, useEffect } from 'react';
+import { MoviePage } from 'types/movie';
 
 function Listing() {
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
-    console.log(response.data);
-  });
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      const data = response.data as MoviePage;
+      setPageNumber(data.number);
+    });
+  }, []);
+
   return (
     <>
       <Pagination />
